9b41b59e22a4beab1525b24d240b96223e5790f2
Uru0310 <64073377+Uru0310@users.noreply.github.com>
2020-09-04 17:30:42 +0900

    post-create

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ee940fd22027bc5246fec6dd30bed5efe02dec6da74da201ec8ef8ad7dae6865f1f66708381d14d2125e71e212d89054daaeea9dffa9d513dfbb25c00efa512d'
